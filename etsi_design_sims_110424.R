9126930440c44fa889ce90a5dc073014bf13cf8c
Rebecca Knowlton <95377946+rebeccaknowlton@users.noreply.github.com>
2025-06-25 15:20:37 -0500

    Add files via upload

--- etsi_design_sims_110424.R
+++ etsi_design_sims_110424.R
@@ -2,15 +2,14 @@ library(etsi)
 library(quantreg)
 
 # only change setting and run everything below
-# also need to load get.truth, get.parameters from the master sims file
-setting <- 2
+# Make sure get.truth and get.parameters are loaded from the master sims file
+setting <- 1
 
 # read in study A from simulations
-setwd("C:/Users/rkkno/Documents/University of Texas at Austin/etsi")
-study.A <- read.table(paste("etsi.studyA",setting,"_110424.txt", sep=""), header = T)
+study.A <- read.table(paste("sim_output/etsi.studyA",setting,"_110424.txt", sep=""), header = T)
 
 # empirical power
-output <- read.table(paste("etsi.output", setting, "_110424",".txt", sep=""), header = T)
+output <- read.table(paste("sim_output/etsi.output", setting, "_110424",".txt", sep=""), header = T)
 emp.power <- apply(output[,c(9,12,15)], 2, function(x) mean(x < 0.05))
 
 thresholds <- c(0.5, 0.6, 0.7)
@@ -38,15 +37,7 @@ results[2,] <- emp.power
 set.seed(1)
 results[3,] <- get.truth(setting)$pi
 results.latex <- format(round(results ,3),nsmall=3)
-latex.table(results.latex, paste0("designsimres", setting), caption = "", dcolumn = T)
-print(round(results,3))
 
-# sample size results
-n.results <- matrix(ncol = 3, nrow = 2)
-rownames(n.results) <- c("Estimated n (from empirical power)", "Estimated n (from estimated power)")  
-colnames(n.results) <- c("Delta.P: k = 0.5", "Delta.P: k = 0.6", "Delta.P: k = 0.7")
-n.results[1,] <- unlist(n.emp.sim)
-n.results[2,] <- unlist(n.est.sim)
-n.results.latex <- format(round(n.results ,3),nsmall=3)
-latex.table(n.results.latex, paste0("n_designsimres", setting), caption = "", dcolumn = T)
-print(round(n.results,3))
+# TABLE 2 output
+latex.table(results.latex, paste0("results/table2_setting", setting), caption = "", dcolumn = T)
+print(round(results,3))
